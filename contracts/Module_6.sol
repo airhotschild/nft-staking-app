ea98e7b4cf59cb140a9d84313d6130e2be50f290
airhotschild <airhotschild@users.noreply.github.com>
2026-06-19 19:21:33 +0300

    chore(contract): handle zero address validation checks (step 27)

--- contracts/Module_6.sol
+++ contracts/Module_6.sol
@@ -6,3 +6,11 @@ contract StakingPart_26 {
     uint256 public constant STEP = 26;
     // Random: rlqon
 }
+// Solidity Module step 27
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract StakingPart_27 {
+    uint256 public constant STEP = 27;
+    // Random: 1wz39l
+}
