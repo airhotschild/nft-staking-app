7c1745a4931b37e348491625de37b5c914deaee8
airhotschild <airhotschild@users.noreply.github.com>
2026-06-19 19:21:32 +0300

    docs(market): add deploy parameters test case (step 2)

--- contracts/Module_1.sol
+++ contracts/Module_1.sol
@@ -6,3 +6,11 @@ contract StakingPart_1 {
     uint256 public constant STEP = 1;
     // Random: 1qlrqk
 }
+// Solidity Module step 2
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract StakingPart_2 {
+    uint256 public constant STEP = 2;
+    // Random: mvvkb
+}
